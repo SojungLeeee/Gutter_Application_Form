bc1b00051410a023027704d8be4f776eddeb882e
SojungLeeee <164296796+SojungLeeee@users.noreply.github.com>
2024-08-08 21:55:56 +0900

    Update bring.js

--- config/bring.js
+++ config/bring.js
@@ -11,7 +11,7 @@ document.getElementById('fetchDataButton').addEventListener('click', async funct
         console.error('Failed to fetch data:', error);
     }
     // 테이블이 있는 컨테이너의 높이 자동 조정
-    dataContainer.style.height = `${table.scrollHeight}px`;
+    // dataContainer.style.height = `${table.scrollHeight}px`;
 });
 
 
@@ -135,88 +135,6 @@ for (let i = 0; i < colortable.length; i++){
     }
 }
 
-
-
-
-// function displayData(data) {
-//     const dataContainer = document.getElementById('dataContainer');
-//     dataContainer.innerHTML = ''; // 기존 콘텐츠 제거
-
-//     if (data.length === 0) {
-//         dataContainer.textContent = 'No data available';
-//         return;
-//     }
-
-//     const table = document.createElement('table');
-//     table.style.width = '100%'; // 테이블 너비를 100%로 설정
-//     table.style.borderCollapse = 'collapse'; // 테이블 테두리 병합
-//     table.style.textAlign = 'center'; // 테이블 전체 가운데 정렬
-//     table.id = "tableid";
-
-//     // 테이블 헤더 생성
-//     const headerRow = document.createElement('tr');
-//     table.innerHTML = `
-//         <tr>
-//             <th>수</th>
-//             <th>이름</th>
-//             <th>성별</th>
-//             <th>학번</th>
-//             <th>전화번호</th>
-//             <th>나이</th>
-//             <th>전공(학과)</th>
-//             <th>제출일시</th>
-//         </tr>
-//     `;
-//     headerRow.style.borderBottom = '2px solid #000'; // 헤더 밑에 두꺼운 구분선 추가
-//     table.appendChild(headerRow);
-
-    
-
-//     // 테이블 행에 데이터 삽입
-//     data.forEach((item, index) => {
-//         const row = table.insertRow();
-//         const cellValues = Object.values(item); // 객체의 값들만 추출
-
-//         // 순번 셀 추가
-//         const numberCell = row.insertCell();
-//         numberCell.textContent = index + 1; // 1부터 시작
-
-//         cellValues.forEach((cellText, i) => {
-//             const cell = row.insertCell();
-//             cell.style.textAlign = 'center'; // 셀 내용 가운데 정렬
-//             if (i === cellValues.length - 1) { // 마지막 셀이 Write Date인 경우
-//                 cell.textContent = formatDateTime(cellText);
-//             } else {
-//                 cell.textContent = cellText !== null && cellText !== undefined ? cellText : '';
-//             }
-//         });
-//         // 각 행 아래에 구분선 추가
-//         row.style.borderBottom = '1px solid #ccc'; // 각 행 아래에 얇은 구분선 추가
-//     });
-
-    
-//         // 테이블의 모든 td 요소를 가져오기
-//     const colortable = document.getElementById('tableid');
-//     const cell = colortable.getElementsByTagName('td');
-    
-//     // 각 셀을 검사하여 성별에 따라 색상 변경
-//     for (let i = 0; i < cell.length; i++) {
-//         if (cell[i].innerText === '남자 ') {
-//             cell[i].style.color = 'blue';  // 남자일 경우 파란색
-//         } else if (cell[i].innerText === '여자 ') {
-//             cell[i].style.color = 'red';   // 여자일 경우 빨간색
-//         }
-//     }
-
-
-//     dataContainer.appendChild(table);
-
-//     // 테이블이 있는 컨테이너의 높이 자동 조정
-//     dataContainer.style.height = `${table.scrollHeight}px`;
-
-
-    
-// }
 
 
 
