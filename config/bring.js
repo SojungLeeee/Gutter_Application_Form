fb2bdd645da128bbc01329ac8f4ccb2998aced2f
SojungLeeee <164296796+SojungLeeee@users.noreply.github.com>
2024-08-12 13:38:21 +0900

    Update bring.js

--- config/bring.js
+++ config/bring.js
@@ -56,7 +56,7 @@ function formatDateTime(dateString) {
     const day = String(date.getUTCDate()).padStart(2, '0');
 
     // 시간을 로컬 시간으로 변환
-    const hours = String(date.getUTCHours()+9).padStart(2, '0'); // 9시간을 더해 KST (한국 표준시)로 변환
+    const hours = String(date.getUTCHours()).padStart(2, '0'); // 9시간을 더해 KST (한국 표준시)로 변환
     const minutes = String(date.getUTCMinutes()).padStart(2, '0');
     const seconds = String(date.getUTCSeconds()).padStart(2, '0');
 
