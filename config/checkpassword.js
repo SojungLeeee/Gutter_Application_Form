afe1939d404279c4e9f375c5c7b6de3e529861ef
SojungLeeee <164296796+SojungLeeee@users.noreply.github.com>
2024-08-13 23:41:24 +0900

    Update checkpassword.js

--- config/checkpassword.js
+++ config/checkpassword.js
@@ -1,7 +1,7 @@
 function checkPassword(event) {
     event.preventDefault(); // 기본 링크 동작을 방지
     const password = prompt("\n관리자 확인이 필요합니다. \n비밀번호를 입력하세요. \n");
-    const correctPassword = "1234";
+    const correctPassword = "gutter1988";
 
 
     if (password===null) {
